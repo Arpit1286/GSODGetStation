9e0d64ef9d78ecb43df63ad9f4aa3b9293b8882e
Arpit1286 <arpit.tandon@ymail.com>
2015-02-26 16:55:51 -0800

    Added JobBuilder Class from Tom White's Hadoop Book repository

--- src/main/java/org/arpit/mapreduce/GetStation.java
+++ src/main/java/org/arpit/mapreduce/GetStation.java
@@ -1,9 +1,35 @@
 package org.arpit.mapreduce;
 
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.ToolRunner;
+
 /**
  * Created by Arpit on 2/25/2015.
  */
-public class GetStation {
+public class GetStation extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = JobBuilder.parseInputAndOutput(this, getConf(),args);
+        if (job == null) {
+            return -1;
+        }
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapperClass(GetStationMapper.class);
+        job.setReducerClass(GetStationReducer.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new GetStation(), args);
+        System.exit(exitCode);
+    }
 
 
 }
